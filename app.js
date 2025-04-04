e928c0a15e0e51206149d7c193296d444900856d
raj kumar <raj445611@gmail.com>
2025-04-04 15:27:53 +0400

    app.js modified: message is changed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("welcome to shopeasy!");
+    res.send("welcome to shopeasy from JVLcode!");
 })
 
 app.listen(PORT, () => {

62305c5fe1ff8b55b32b98749e960a1d09f288e4
raj kumar <raj445611@gmail.com>
2025-04-11 16:40:19 +0400

    App.js: Change in Message

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const name = "CodeTest";
 
 app.get("/", (req, res) => {
-    res.send("welcome to shopeasy from JVLcode!");
+    const message = `welcome to shopeasy from ${name}!`
+    res.send(message);
 })
 
 const server = app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
 })
 
-module.exports = {app,server}
+module.exports = {app,server, name}
